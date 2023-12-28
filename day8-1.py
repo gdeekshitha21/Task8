7628c7650266e90a30914abddc7e46bd724805ca
gdeekshitha21 <154420107+gdeekshitha21@users.noreply.github.com>
2023-12-28 16:23:27 +0530

    Add files via upload

---
+++ day8-1.py
@@ -0,0 +1,7 @@
+class Circle:
+    def init__(self, my_list):
+        self.my_list=my_list
+    def print_list(self):
+        print(self.my_list)
+circle=Circle([10, 501, 22, 37, 100, 999, 87, 351])
+circle.print_list()
